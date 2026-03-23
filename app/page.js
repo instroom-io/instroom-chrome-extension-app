05e3e0dc578210985fc807d0499d252ddcda325f
herotapalesdelacruz-Ui <herotapalesdelacruz@gmail.com>
2026-03-23 13:41:27 +0800

    .03202026 -update on home page ui

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-white dark:bg-zinc-950">
       {/* Header */}
+      h1
       <header className="sticky top-0 z-50 w-full border-b border-zinc-200 bg-white/80 backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-950/80">
         <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
           <div className="flex items-center gap-2 font-bold text-xl">
